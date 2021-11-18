4ea0646d6e487504893c51d9b90445a3431a8343
Ed Halliwell <edhalliwell@hotmail.com>
2021-11-18 16:45:48 +0000

    Added moment and fixed createdAt formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import dotenv from "dotenv";
+import moment from "moment";
 
 dotenv.config();
 
@@ -15,7 +16,7 @@ export default function App(): JSX.Element {
   const [isFirstLoad, setIsFirstLoad] = useState(true);
   // const [message, setMessage] = useState<string>();
   const [todoData, setTodoData] = useState<Todo[]>([]);
-  const [formValue, setFormValue] = useState<string>("");
+  const [newTodoValue, setNewTodoValue] = useState<string>("");
 
   const loadDataFromEndpoint = async (endpoint: string) => {
     try {
@@ -38,11 +39,12 @@ export default function App(): JSX.Element {
     }
   });
 
-  const createTodo = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleCreateTodo = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     await axios
       .post(`${process.env.REACT_APP_PROD_API_URL}todos`, {
-        text: formValue,
+        text: newTodoValue,
+        createdAt: Date.now(),
       })
       .then(function (response) {
         todoData.push(response.data);
@@ -50,7 +52,7 @@ export default function App(): JSX.Element {
       .catch(function (error) {
         console.log(error);
       });
-    setFormValue("");
+    setNewTodoValue("");
   };
 
   const handleDeleteTodo = async (todoId: number) => {
@@ -65,29 +67,64 @@ export default function App(): JSX.Element {
       });
   };
 
+  const handleIsCompleteToggle = async (
+    todoId: number,
+    currentCompletedValue: boolean
+  ) => {
+    await axios
+      .patch(`${process.env.REACT_APP_PROD_API_URL}todos/${todoId}`, {
+        completed: !currentCompletedValue,
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const timestampConverter = (unixTimestamp: number): string => {
+    const array = moment(unixTimestamp).toArray(); // [2013, 1, 4, 14, 40, 16, 154];
+    const outputString = moment([
+      array[0],
+      array[1],
+      array[2],
+      array[3],
+      array[4],
+    ]).fromNow();
+    return outputString;
+  };
+
   return (
     <>
-      <h1>This is a title</h1>
-      <ul>
-        {todoData.map((todo) => (
-          <li key={todo.id}>
-            {todo.text}
-            <button>Edit</button>
-            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      <h1>Todo List</h1>
       <section>
-        <h3>Create new todo:</h3>
-        <form onSubmit={createTodo}>
+        <h3>Create a new todo:</h3>
+        <form onSubmit={handleCreateTodo}>
           <input
-            value={formValue}
-            onChange={(e) => setFormValue(e.target.value)}
+            value={newTodoValue}
+            onChange={(e) => setNewTodoValue(e.target.value)}
             placeholder="Write your todo..."
           />
           <button type="submit">Create</button>
         </form>
       </section>
+      <hr />
+      <ul>
+        {todoData.map((todo) => (
+          <li key={todo.id}>
+            {todo.text} - {timestampConverter(todo.createdAt)}
+            <button>Edit</button>
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            <label>
+              <input
+                type="checkbox"
+                name="isComplete"
+                defaultChecked={todo.completed}
+                onChange={() => handleIsCompleteToggle(todo.id, todo.completed)}
+              />
+              Completed?
+            </label>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
